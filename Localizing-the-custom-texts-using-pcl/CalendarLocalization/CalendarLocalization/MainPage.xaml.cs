869fe624bd9e1780202912cf8e804a0731361ce3
syncfusion <muniappan.subramanian@syncfusion.com>
2018-12-26 18:03:53 +0530

    Localization Sample's Committed.

---
+++ Localizing-the-custom-texts-using-pcl/CalendarLocalization/CalendarLocalization/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using Syncfusion.SfCalendar.XForms;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace CalendarLocalization
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+
+            CalendarResourceManager.Manager = new System.Resources.ResourceManager("CalendarLocalization.Resources.Syncfusion.SfCalendar.XForms", GetType().GetTypeInfo().Assembly);
+            CultureInfo.CurrentUICulture = new CultureInfo("pt");
+            calendar.Locale = new System.Globalization.CultureInfo("pt-PT");
+
+            calendar.MonthViewSettings = new MonthViewSettings()
+            {
+                SelectedDayTextColor= Color.White,
+                TodayTextColor = Color.FromHex("#007CEE")
+
+            };
+
+            // Creating Events in SfCalendar
+            CalendarEventCollection calendarInlineEvents = new CalendarEventCollection();
+
+            calendarInlineEvents.Add(new CalendarInlineEvent()
+            {
+                Subject = "Goto Meeting",
+                StartTime = DateTime.Today.AddHours(10),
+                EndTime = DateTime.Today.AddHours(12),
+                IsAllDay = true,
+            });
+
+            calendar.DataSource = calendarInlineEvents;
+        }
+	}
+}
